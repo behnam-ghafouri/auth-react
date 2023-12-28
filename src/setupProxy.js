5c3e882ee906fdcb8c5c7ea124fdfe1967b42afe
behnam <behnam.ghafouri@gmail.com>
2023-12-28 11:28:12 -0500

    update ip

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://localhost', // Change the port if needed
+      target: 'https://20.246.194.163', // Change the port if needed
       changeOrigin: true,
       secure: false,
       // pathRewrite: {
